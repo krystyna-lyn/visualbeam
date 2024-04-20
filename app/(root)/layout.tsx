cd1286aaf416e7193f1219d4472b752fa48c36c5
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-04-20 16:31:34 +0200

    add pages, sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,22 @@
-const RootLayout = () => {
+import Navbar from "@/components/Navbar"
+import Sidebar from "@/components/Sidebar"
+import { ReactNode } from "react"
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>RootLayout</div>
+        <main className="relative">
+            <Navbar />
+            <div className="flex">
+                <Sidebar />
+                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pt-14 sm:px-14">
+                    <div className="w-full">
+                        {children}
+                    </div>
+                </section>
+            </div>
+
+            Footer
+        </main>
     )
 }
 export default RootLayout
