b4db5520b3449eb174aba8b54677acd7af8d8a12
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-04-25 20:34:11 +0100

    update Navbar, layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,22 +1,25 @@
 import Navbar from "@/components/Navbar"
 import Sidebar from "@/components/Sidebar"
+import { ClerkProvider } from "@clerk/nextjs"
 import { ReactNode } from "react"
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <main className="relative">
-            <Navbar />
-            <div className="flex">
-                <Sidebar />
-                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pt-14 sm:px-14">
-                    <div className="w-full">
-                        {children}
-                    </div>
-                </section>
-            </div>
+        <ClerkProvider>
+            <main className="relative">
+                <Navbar />
+                <div className="flex">
+                    <Sidebar />
+                    <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pt-14 sm:px-14">
+                        <div className="w-full">
+                            {children}
+                        </div>
+                    </section>
+                </div>
 
-            Footer
-        </main>
+                Footer
+            </main>
+        </ClerkProvider>
     )
 }
 export default RootLayout
