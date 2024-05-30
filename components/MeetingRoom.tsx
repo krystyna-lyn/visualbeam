5451b7908516aee60bb76ec08fe7447f0a90bc99
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-05-30 17:45:32 +0100

    Create MeetingRoom.tsx

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const MeetingRoom = () => {
+    return (
+        <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+            <div className="relative flex size-full items-center justify-center">
+                <div className=" flex size-full max-w-[1000px] items-center">
+
+                </div>
+            </div>
+
+            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+
+                <div className="flex items-center">
+
+                </div>
+            </div>
+
+            <button>
+                <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+
+                </div>
+            </button>
+
+        </section>
+    )
+}
+
+export default MeetingRoom
