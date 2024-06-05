8426ac19fc6574053db9bf76beaadd5ec43fe402
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-05 23:27:44 +0100

    update meeting page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,14 +1,39 @@
+'use client';
+
+import { useUser } from "@clerk/nextjs"
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import { useState } from "react";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useParams } from "next/navigation";
 
 
 
-const MeetingRoom = () => {
+const Meeting = ({ params }: { params: { id: string } }) => {
+
+    const { id } = useParams();
+    const { isLoaded, user } = useUser();
+    const [isSetupComplete, setIsSetupComplete] = useState(false)
+    const { call, isCallLoading } = useGetCallById(id);
+
+
 
     return (
         <main className="h-screen w-full">
+            <StreamCall call={call}>
+                <StreamTheme>
 
+                    {!isSetupComplete ? (
+                        <MeetingSetup />
+                    ) : (
+                        <MeetingRoom />
+                    )}
+                </StreamTheme>
+            </StreamCall>
         </main>
     )
 }
 
-export default MeetingRoom
+export default Meeting
 
