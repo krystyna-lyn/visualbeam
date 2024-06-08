9d9421f49fccabd46051038f94b52aabf14d293b
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-08 21:41:11 +0100

    update meeting

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,6 +7,8 @@ import MeetingRoom from "@/components/MeetingRoom";
 import { useState } from "react";
 import { useGetCallById } from "@/hooks/useGetCallById";
 import { useParams } from "next/navigation";
+import { Loader } from "lucide-react";
+import Alert from "@/components/Alert";
 
 
 
@@ -17,7 +19,17 @@ const Meeting = ({ params }: { params: { id: string } }) => {
     const [isSetupComplete, setIsSetupComplete] = useState(false)
     const { call, isCallLoading } = useGetCallById(id);
 
+    if (!isLoaded || isCallLoading) return <Loader />;
 
+    if (!call) return (
+        <p className="text-center text-3xl font-bold text-white">
+            Call Not Found
+        </p>
+    );
+
+    const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+    if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
 
     return (
         <main className="h-screen w-full">
---
+++ components/Alert.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+interface PermissionCardProps {
+    title: string;
+    iconUrl?: string;
+}
+
+
+const Alert = ({ title }: PermissionCardProps) => {
+    return (
+        <div>Alert</div>
+    )
+}
+
+export default Alert
