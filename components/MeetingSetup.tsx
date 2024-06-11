dc6499ea28874021a2014492dc307523d4819bf1
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-11 20:58:46 +0100

    Update MeetingSetup.tsx

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import {
+    DeviceSettings,
+    VideoPreview,
+    useCall,
+    useCallStateHooks,
+} from '@stream-io/video-react-sdk';
+import { useEffect, useState } from 'react';
+import { Button } from './ui/button';
 
 const MeetingSetup = () => {
+    const [isMicToggled, setIsMicToggled] = useState(false)
+    const call = useCall();
+
+    useEffect(() => {
+        if (isMicToggled) {
+            call?.camera.disable();
+            call?.microphone.disable();
+        } else {
+            call?.camera.enable();
+            call?.microphone.enable();
+        }
+
+    }, [isMicToggled, call?.camera, call?.microphone])
+
     return (
-        <div>MeetingSetup</div>
+        <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+            <h1 className="text-center text-2xl font-bold">Setup</h1>
+            <VideoPreview />
+            <div className="flex h-16 items-center justify-center gap-3">
+                <label className="flex items-center justify-center gap-2 font-medium">
+                    <input
+                        type="checkbox"
+                        checked={isMicToggled}
+                        onChange={(e) => setIsMicToggled(e.target.checked)}
+                    />
+                    mic and camera off
+                </label>
+                <DeviceSettings />
+            </div>
+            <Button className='rounded-md bg-green-700 px-4 py-2.5'>
+                Join Meeting
+            </Button>
+        </div>
     )
 }
 
