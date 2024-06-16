614418a752483cd517c0ab49db1387d82a971822
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-16 22:41:42 +0100

    update EndCallButton

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { cn } from '@/lib/utils';
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
+import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
 import {
     DropdownMenu,
@@ -10,11 +10,19 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { LayoutList } from 'lucide-react';
+import { LayoutList, Users } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+import EndCallButton from './EndCallButton';
+
+
 
 type CallLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
 
 const MeetingRoom = () => {
+
+
+    const searchParams = useSearchParams();
+    const isPersonalRoom = !!searchParams.get('personal');
 
     const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
     const [showParticipants, setShowParticipants] = useState(false)
@@ -71,8 +79,15 @@ const MeetingRoom = () => {
 
                         </DropdownMenuContent>
                     </div>
-
                 </DropdownMenu>
+                <CallStatsButton />
+                <button onClick={() => setShowParticipants((prev) => !prev)}>
+                    <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
+                        <Users size={20} className="text-white" />
+                    </div>
+                </button>
+                {!isPersonalRoom && <EndCallButton />}
+
             </div>
 
 
