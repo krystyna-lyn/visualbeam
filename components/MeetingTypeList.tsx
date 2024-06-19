464bc941217f76c18fcd3b1d410815acdadced92
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-19 19:00:14 +0100

    Update MeetingTypeList

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,8 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useUser } from '@clerk/nextjs';
 import Loader from './Loader';
 import { useToast } from './ui/use-toast';
+import { Textarea } from './ui/textarea';
+import ReactDatePicker from 'react-datepicker'
 
 
 const initialValues = {
@@ -75,6 +77,7 @@ const MeetingTypeList = () => {
         description="Start an instant meeting"
         handleClick={() => setMeetingState('isInstantMeeting')}
       />
+
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
@@ -108,12 +111,27 @@ const MeetingTypeList = () => {
             <label className="text-base font-normal leading-[22.4px] text-sky-2">
               Add a description
             </label>
-
+            <Textarea
+              className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+              onChange={(e) =>
+                setValues({ ...values, description: e.target.value })
+              }
+            />
           </div>
           <div className="flex w-full flex-col gap-2.5">
             <label className="text-base font-normal leading-[22.4px] text-sky-2">
               Select Date and Time
             </label>
+            <ReactDatePicker
+              selected={values.dateTime}
+              onChange={(date) => setValues({ ...values, dateTime: date! })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+            />
 
           </div>
         </MeetingModal>
