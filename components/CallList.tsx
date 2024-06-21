51faafa0363ec28bd7344ba45dd4626e26869cc5
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-21 18:52:23 +0100

    update CallList

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { useGetCalls } from '@/hooks/useGetCalls'
-import { CallRecording } from '@stream-io/video-react-sdk';
+import { Call, CallRecording } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
+import MeetingCard from './MeetingCard';
 
 
 const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
@@ -22,8 +23,35 @@ const CallList = ({ type }: { type: 'upcoming' | 'ended' | 'recordings' }) => {
         }
     }
 
+    const getNoCallsMessage = () => {
+        switch (type) {
+            case 'ended':
+                return 'No previous Calls';
+            case 'recordings':
+                return 'No srecordings';
+            case 'upcoming':
+                return 'No upcoming Calls';
+            default:
+                return [];
+        }
+    }
+
+    const calls = getCalls();
+    const noCallsMessage = getNoCallsMessage()
+
     return (
-        <div>CallList</div>
+        <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
+            {calls && calls.length > 0 ? calls.map(
+                (meeting: Call | CallRecording) => (
+                    <MeetingCard />
+                )) :
+                (
+                    <h1 className="text-2xl font-bold text-white">
+                        {noCallsMessage}
+                    </h1>
+                )
+            }
+        </div>
     )
 }
 
