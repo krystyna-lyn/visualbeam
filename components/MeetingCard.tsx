c6be184f2cdeb719c87ca2485f3a0e52ab46c1c2
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-24 17:09:37 +0100

    update MeetingCard

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -1,4 +1,8 @@
+import { avatarImages } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
 
 interface MeetingCardProps {
     title: string;
@@ -24,6 +28,54 @@ const MeetingCard = ({ title, date, icon, isPreviousMeeting, buttonIcon1, button
                     </div>
                 </div>
             </article>
+            <article className={cn("flex justify-center relative", {})}>
+                <div className="relative flex w-full max-sm:hidden">
+                    {avatarImages.map((img, index) => (
+                        <Image
+                            key={index}
+                            src={img}
+                            alt="attendees"
+                            width={40}
+                            height={40}
+                            className={cn("rounded-full", { absolute: index > 0 })}
+                            style={{ top: 0, left: index * 28 }}
+                        />
+                    ))}
+
+                    <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
+                        +5
+                    </div>
+                </div>
+                {!isPreviousMeeting && (
+                    <div className="flex gap-2">
+                        <Button onClick={handleClick} className="rounded bg-blue-1 px-6">
+                            {buttonIcon1 && (
+                                <Image src={buttonIcon1} alt="feature" width={20} height={20} />
+                            )}
+                            &nbsp; {buttonText}
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                navigator.clipboard.writeText(link);
+                                toast({
+                                    title: "Link Copied",
+                                });
+                            }}
+                            className="bg-dark-4 px-6"
+                        >
+                            <Image
+                                src="/icons/copy.svg"
+                                alt="feature"
+                                width={20}
+                                height={20}
+                            />
+                            &nbsp; Copy Link
+                        </Button>
+                    </div>
+                )}
+            </article>
+
+
         </section>
     )
 }
