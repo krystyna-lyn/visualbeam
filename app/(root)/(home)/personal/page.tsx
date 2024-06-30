c8f53b93816d65a32694570f30514cf76a6f21c0
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2024-06-30 22:29:40 +0100

    update personal room

--- app/(root)/(home)/personal/page.tsx
+++ app/(root)/(home)/personal/page.tsx
@@ -1,5 +1,13 @@
 'use client'
+
+import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
+import { useGetCallById } from '@/hooks/useGetCallById';
 import { useUser } from '@clerk/nextjs';
+import { useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useRouter } from 'next/navigation';
+
+
 
 
 const Table = ({ title, description }: { title: string; description: string; }) => (
@@ -7,26 +15,64 @@ const Table = ({ title, description }: { title: string; description: string; }) 
         <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
             {title}
         </h1>
-        <p className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
+        <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
             {description}
-        </p>
+        </h1>
     </div>
 )
 
 const PersonalRoom = () => {
 
     const { user } = useUser();
+    const { toast } = useToast();
+    const client = useStreamVideoClient();
+    const router = useRouter();
+
+
+    const meetingId = user?.id; // meetingId is the user's id
+    const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
+
+    const startRoom = async () => {
+        if (!client || !user) return;
+        const newCall = client.call("default", meetingId!);
+
+        if (!call) {
+            await newCall.getOrCreate({
+                data: {
+                    starts_at: new Date().toISOString(),
+                },
+            });
+        }
+        router.push(`/meeting/${meetingId}?personal=true`);
+    }
+
+    const { call } = useGetCallById(meetingId!);
 
     return (
         <section className='size-full flex flex-col gap-10 text-white'>
             <h1 className='text-3xl text-bold'>
-                Personal Room
-                <div className="flex flex-col w-full gap-10 text-white">
-                    <Table title='topic' description={`${user?.username}'s Meeting Room`} />
 
+                <div className="flex flex-col w-full gap-10 text-white">
+                    <Table title='Topic' description={`${user?.username}'s meeting room`} />
+                    <Table title='Meeting ID' description={`${meetingId}`} />
+                    <Table title='Invite Link' description={`${meetingLink}`} />
+                </div>
+                <div className='flex gap-5'>
+                    <Button className='bg-blue-1' onClick={startRoom}>
+                        Start Meeting
+                    </Button>
+                    <Button className='bg-dark-3'
+                        onClick={() => {
+                            navigator.clipboard.writeText(meetingLink);
+                            toast({
+                                title: "Link Copied",
+                            });
+                        }}>
+                        Copy Invitation
+                    </Button>
                 </div>
             </h1>
-        </section>
+        </section >
     )
 }
 
